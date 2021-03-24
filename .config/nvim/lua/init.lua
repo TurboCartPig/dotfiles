7754db81bc1aa311df2c256ac6320b9b6f5060e0
Dennis Kristiansen <denniskristiansen@protonmail.com>
2021-03-24 14:06:08 +0100

    Fix indent

--- .config/nvim/lua/init.lua
+++ .config/nvim/lua/init.lua
@@ -14,14 +14,14 @@ setmetatable(kind_labels, kind_labels_mt)
 -- setup lsp_status line
 lsp_status.register_progress()
 lsp_status.config {
-  kind_labels = kind_labels,
-  indicator_errors = "×",
-  indicator_warnings = "!",
-  indicator_info = "i",
-  indicator_hint = "›",
-  -- the default is a wide codepoint which breaks absolute and relative
-  -- line counts if placed before airline's Z section
-  status_symbol = "",
+	kind_labels = kind_labels,
+	indicator_errors = "×",
+	indicator_warnings = "!",
+	indicator_info = "i",
+	indicator_hint = "›",
+	-- the default is a wide codepoint which breaks absolute and relative
+	-- line counts if placed before airline's Z section
+	status_symbol = "",
 }
 
 -- Neovim lsp config ----------------------------------------------------------------- {{{1

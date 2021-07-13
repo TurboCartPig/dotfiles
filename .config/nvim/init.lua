37357fd307e590503468c45238cd1e74be1c6a67
Dennis Kristiansen <denniskristiansen@protonmail.com>
2021-07-13 03:39:13 +0200

    Fix colorscheme overrides ...again

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -541,30 +541,37 @@ autopairs.setup {
 
 -- highlights and colorscheme ------------------------------------------------------- {{{1
 
-vim.cmd [[colorscheme gruvbox]]
+-- Setup auto commands for overriding highlight when gruvbox is set as the colorscheme.
+-- This fixes issues with the colorscheme plugin I use.
+-- And it makes it so I can flip between different colorschemes, while my gruvbox overrides only effect gruvbox.
 
 -- 1. Clear annoying colors
 -- 2. Override some poor defaults and correct omissions from colorscheme
 vim.cmd [[
-	highlight Cursor            gui=NONE   guibg=#FB4632 guifg=NONE
-	highlight SignColumn        guibg=none
-	highlight Folded            guibg=none
-	highlight FoldColumn        guibg=none
-	highlight StatusLine        guibg=none guifg=#292929
-	highlight StatusLineNC      guibg=none guifg=#292929
-	highlight GruvboxAquaSign   guibg=none
-	highlight GruvboxBlueSign   guibg=none
-	highlight GruvboxGreenSign  guibg=none
-	highlight GruvboxOrangeSign guibg=none
-	highlight GruvboxPurpleSign guibg=none
-	highlight GruvboxRedSign    guibg=none
-	highlight GruvboxYellowSign guibg=none
+	augroup ColorOverrides
+		autocmd!
+		autocmd ColorScheme gruvbox highlight  Cursor            gui=NONE   guibg=#FB4632 guifg=NONE
+		autocmd ColorScheme gruvbox highlight  SignColumn        guibg=none
+		autocmd ColorScheme gruvbox highlight  Folded            guibg=none
+		autocmd ColorScheme gruvbox highlight  FoldColumn        guibg=none
+		autocmd ColorScheme gruvbox highlight  StatusLine        guibg=none guifg=#292929
+		autocmd ColorScheme gruvbox highlight  StatusLineNC      guibg=none guifg=#292929
+		autocmd ColorScheme gruvbox highlight  GruvboxAquaSign   guibg=none
+		autocmd ColorScheme gruvbox highlight  GruvboxBlueSign   guibg=none
+		autocmd ColorScheme gruvbox highlight  GruvboxGreenSign  guibg=none
+		autocmd ColorScheme gruvbox highlight  GruvboxOrangeSign guibg=none
+		autocmd ColorScheme gruvbox highlight  GruvboxPurpleSign guibg=none
+		autocmd ColorScheme gruvbox highlight  GruvboxRedSign    guibg=none
+		autocmd ColorScheme gruvbox highlight  GruvboxYellowSign guibg=none
 
-	highlight  link Operator     GruvboxRed
-	highlight  link NormalFloat  GruvboxFg0
-	highlight! link DiffAdd      GruvboxGreenSign
-	highlight! link DiffChange   GruvboxPurpleSign
-	highlight! link DiffDelete   GruvboxRedSign
+		autocmd ColorScheme gruvbox highlight  link Operator     GruvboxRed
+		autocmd ColorScheme gruvbox highlight  link NormalFloat  GruvboxFg0
+		autocmd ColorScheme gruvbox highlight! link DiffAdd      GruvboxGreenSign
+		autocmd ColorScheme gruvbox highlight! link DiffChange   GruvboxPurpleSign
+		autocmd ColorScheme gruvbox highlight! link DiffDelete   GruvboxRedSign
+	augroup END
 ]]
+
+vim.cmd [[colorscheme gruvbox]]
 
 -- vi: foldmethod=marker

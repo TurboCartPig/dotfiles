7eb1bf11dbb00ba6dca3310dad43f5446d6d80ab
Dennis Kristiansen <denniskristiansen@protonmail.com>
2022-06-09 02:38:14 +0200

    emacs: Add initial emacs config

---
+++ .emacs.d/init.el
@@ -0,0 +1,160 @@
+;;; Set Emacs settings  -*- lexical-binding: t; -*-
+
+;; Reduce the frequency of garbage collection by making it happen on
+;; each 50MB of allocated data (the default is on every 0.76MB)
+(setq gc-cons-threshold 50000000)
+
+;; Warn when opening files bigger than 100MB
+(setq large-file-warning-threshold 100000000)
+
+;;; Tweak UI elements
+
+;; Set fonts
+(set-face-attribute 'default nil :font "FiraCode NF" :height 135)
+
+;; Disable the default startup screen
+(setq inhibit-startup-message t)
+;; Disable scrollbar
+(scroll-bar-mode -1)
+;; Disable menu bar
+(menu-bar-mode -1)
+;; Disable tool bar
+(tool-bar-mode -1)
+;; Disable tooltips
+(tooltip-mode -1)
+;; Set the fringe/padding around the window to a few pixels
+(set-fringe-mode 4)
+;; Enable line numbers
+(global-display-line-numbers-mode 1)
+;; Highlight the cursor line
+(global-hl-line-mode 1)
+
+
+;;; Keyboard settings
+
+;; <escape> quits prompts
+(global-set-key (kbd "<escape>") 'keyboard-escape-quit)
+
+;;; Packages
+
+;; Add melpa as a package repo
+(require 'package)
+
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+(package-initialize)
+
+;; Refresh the package archives if empty
+(unless package-archive-contents
+  (package-refresh-contents))
+
+;; Install use-package if not installed already
+(unless (package-installed-p 'use-package)
+  (package-install 'use-package))
+
+;; Define all the packages I use
+(require 'use-package)
+
+(use-package evil
+  :ensure t
+  :init
+  (setq evil-want-integration t
+        evil-want-keybinding nil)
+  :config
+  (evil-mode 1))
+
+(use-package evil-collection
+  :after evil
+  :ensure t
+  :config
+  (evil-collection-init))
+
+(use-package which-key
+  :ensure t
+  :init
+  (setq which-key-allow-evil-operators t)
+  :config
+  (which-key-mode 1))
+
+(use-package company
+  :ensure t
+  :config
+  (global-company-mode))
+
+(use-package parinfer-rust-mode
+  :ensure t
+  :hook emacs-lisp-mode
+  :init
+  (setq parinfer-rust-auto-download t))
+
+(use-package rainbow-delimiters
+  :ensure t
+  :hook emacs-lisp-mode)
+
+(use-package all-the-icons
+  :ensure t)
+
+(use-package doom-themes
+  :ensure t
+  :config
+  (setq doom-themes-enable-bold t
+        doom-themes-enable-italic t)
+  (load-theme 'doom-one t)
+  ;; Enable flashing mode-line on errors
+  (doom-themes-visual-bell-config)
+  ;; Corrects (and improves) org-mode's native fontification.
+  (doom-themes-org-config))
+
+(use-package doom-modeline
+  :ensure t
+  :init
+  (setq doom-modeline-minor-modes t)
+  :hook (after-init . doom-modeline-mode))
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(company rainbow-delimiters doom-modeline all-the-icons which-key doom-themes evil-collection parinfer-rust-mode use-package evil)))
+(custom-set-faces)
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ 
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ 
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ 
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ 
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ 
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ 
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ 
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ 

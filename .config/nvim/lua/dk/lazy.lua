2f1768ba80a8a55ad5151e690f8e9a9e214419a9
Dennis Kristiansen <dennis.kristiansen@tietoevry.com>
2025-01-10 10:56:56 +0100

    nvim: Remove redundant plugins

--- .config/nvim/lua/dk/lazy.lua
+++ .config/nvim/lua/dk/lazy.lua
@@ -1,82 +1,79 @@
 -- Bootstrap lazy.nvim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-    local lazyrepo = "https://github.com/folke/lazy.nvim.git"
-    local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
-    if vim.v.shell_error ~= 0 then
-        vim.api.nvim_echo({
-            { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
-            { out,                            "WarningMsg" },
-            { "\nPress any key to exit..." },
-        }, true, {})
-        vim.fn.getchar()
-        os.exit(1)
-    end
+	local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+	local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+	if vim.v.shell_error ~= 0 then
+		vim.api.nvim_echo({
+			{ "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+			{ out,                            "WarningMsg" },
+			{ "\nPress any key to exit..." },
+		}, true, {})
+		vim.fn.getchar()
+		os.exit(1)
+	end
 end
 vim.opt.rtp:prepend(lazypath)
 
 -- Setup lazy.nvim
 require("lazy").setup({
-    spec = {
-        -- Import your plugins
-        { import = "dk.plugins" },
+	spec = {
+		-- Import your plugins
+		{ import = "dk.plugins" },
 
-        { "tpope/vim-surround",            lazy = false },
-        { "tpope/vim-commentary",          lazy = false },
-        { "tpope/vim-repeat",              lazy = false },
-        { "airblade/vim-rooter",           lazy = false },
-        { "wellle/targets.vim",            lazy = false },
+		{ "tpope/vim-surround",             lazy = false },
+		{ "tpope/vim-commentary",           lazy = false },
+		{ "tpope/vim-repeat",               lazy = false },
+		{ "airblade/vim-rooter",            lazy = false },
+		{ "wellle/targets.vim",             lazy = false },
+		{ "christoomey/vim-tmux-navigator", lazy = false },
 
-        { "sbdchd/neoformat" },
-        { "editorconfig/editorconfig-vim" },
-        { "christoomey/vim-tmux-navigator" },
+		{ "ryanoasis/vim-devicons",         lazy = false },
+		{ "kyazdani42/nvim-web-devicons",   lazy = false },
+		{ "akinsho/org-bullets.nvim" },
+		{ "lukas-reineke/headlines.nvim" },
+		{ "akinsho/nvim-bufferline.lua",    lazy = false },
+		{ "ellisonleao/gruvbox.nvim",       lazy = false, config = true },
+		{ "stevearc/dressing.nvim",         config = true },
+		{
+			"chentoast/marks.nvim",
+			opts = {
+				default_mappings = true,
+				refresh_interval = 500,
+			}
+		},
+		{
+			"rcarriga/nvim-notify",
+			config = function()
+				vim.notify = require "notify"
+			end,
+		},
 
-        { "ryanoasis/vim-devicons",        lazy = false },
-        { "kyazdani42/nvim-web-devicons",  lazy = false },
-        { "akinsho/org-bullets.nvim" },
-        { "lukas-reineke/headlines.nvim" },
-        { "akinsho/nvim-bufferline.lua",   lazy = false },
-        { "ellisonleao/gruvbox.nvim",      lazy = false, config = true },
-        { "stevearc/dressing.nvim",        config = true },
-        {
-            "chentoast/marks.nvim",
-            opts = {
-                default_mappings = true,
-                refresh_interval = 500,
-            }
-        },
-        {
-            "rcarriga/nvim-notify",
-            config = function()
-                vim.notify = require "notify"
-            end,
-        },
-
-        {
-            "eraserhd/parinfer-rust",
-            ft = { "lisp", "clojure", "fennel" },
-            build = "cargo build --release",
-            enabled = false,
-        },
-        {
-            "kristijanhusak/orgmode.nvim",
-            enabled = false,
-        },
-        {
-            "fatih/vim-go",
-            ft = { "go", "gomod", "gosum", "godoc" },
-            enabled = false,
-        },
-        {
-            "rust-lang/rust.vim",
-            cmd = { "Cargo" },
-            ft = { "rust" },
-            enabled = false,
-        },
-    },
-    -- Configure any other settings here. See the documentation for more details.
-    -- Colorscheme that will be used when installing plugins.
-    install = { colorscheme = { "gruvbox" } },
-    -- Automatically check for plugin updates
-    checker = { enabled = false },
+		{
+			"eraserhd/parinfer-rust",
+			ft = { "lisp", "clojure", "fennel" },
+			build = "cargo build --release",
+			enabled = false,
+		},
+		{
+			"kristijanhusak/orgmode.nvim",
+			enabled = false,
+		},
+		{
+			"fatih/vim-go",
+			ft = { "go", "gomod", "gosum", "godoc" },
+			enabled = false,
+		},
+		{
+			"rust-lang/rust.vim",
+			cmd = { "Cargo" },
+			ft = { "rust" },
+			enabled = false,
+		},
+	},
+	-- Configure any other settings here. See the documentation for more details.
+	-- Colorscheme that will be used when installing plugins.
+	install = { colorscheme = { "gruvbox" } },
+	-- Automatically check for plugin updates
+	checker = { enabled = false },
 })

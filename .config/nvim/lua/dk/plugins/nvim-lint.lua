44d0d6544f9c9665a614fbbb7c04d9f3b43f4cfa
Dennis Kristiansen <dennis.kristiansen@tietoevry.com>
2025-01-13 15:44:14 +0100

    nvim: Add nvim-lint plugin

---
+++ .config/nvim/lua/dk/plugins/nvim-lint.lua
@@ -0,0 +1,22 @@
+return {
+	"mfussenegger/nvim-lint",
+	ft = { "lua", "sh", "bash" },
+	opts = {
+		lua = { "selene" },
+		sh = { "shellcheck" },
+		bash = { "shellcheck" },
+	},
+	config = function(_, opts)
+		local lint = require "lint"
+
+		lint.linters_by_ft = opts
+
+		local group = vim.api.nvim_create_augroup("NvimLint", { clear = true })
+		vim.api.nvim_create_autocmd({ "BufReadPost", "BufWritePost" }, {
+			group = group,
+			callback = function()
+				lint.try_lint()
+			end,
+		})
+	end,
+}
